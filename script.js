9361a4d26c17d2db2dc6d05a6c58cbe62928bce8
j-tew <justinrtew@gmail.com>
2023-10-04 21:13:46 -0500

    add help command

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+// Command list
+const commands = {
+  help: `These are the allowed commands:\n
+    'help' -> print this dialog`,
+  sudo: null
+}
+
+const validCmds = Object.keys(commands);
+const cmdInput = document.getElementById('command');
+
 // Keep command line focused
 document.body.addEventListener('mouseup', () => {
-  input.focus();
+  cmdInput.focus();
 });
 
 // Accept a command
-const input = document.getElementById('command');
+const cmdLines = document.getElementsByName('terminal');
 
-input.addEventListener('keypress', (event) => {
+cmdInput.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    console.log(input.value);
+    let cmd = cmdInput.value
+    if (validCmds.includes(cmd)) {
+      let result = document.createElement('div');
+      result.innerHTML = `<pre>${commands[cmd]}</pre>`;
+      cmdInput.setAttribute('value', '');
+      document.body.append(result);
+      console.log(cmdLines);
+      let lastLine = cmdLines.item(length-1);
+      let nextLine = document.createElement('div');
+      nextLine.innerHTML = lastLine.outerHTML;
+      document.body.append(nextLine);
+    }
   }
 });
