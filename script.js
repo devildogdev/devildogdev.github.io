cc84a40679c2d3000a4ce769d01ebf56fd91ef18
j-tew <justinrtew@gmail.com>
2023-10-14 23:22:34 -0500

    Finally making some progress
    Made generating terminal lines functional and consistent plus some
    cleanup

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Generating the output of the command
+const renderOutput = (cmd) => {
+  let outputText = commands[cmd];
+  let outputDiv = document.createElement('div');
+  outputDiv.setAttribute('id', 'output');
+  outputDiv.innerHTML = outputText;
+  document.body.append(outputDiv);
+}
+
+// Add new line for the terminal
+const mkTerminal = () => {
+  let newTermHTML = `
+    <span id="prompt"><span class="cyan">~</span><span class="green">❯</span></span>
+    <span><textarea id="command" rows="1" cols="22" autofocus></textarea></span>
+  `;
+  let newTermDiv = document.createElement('div');
+  newTermDiv.setAttribute('name', 'terminal');
+  newTermDiv.innerHTML = newTermHTML;
+  document.body.append(newTermDiv);
+}
+
+// New terminal on load
+mkTerminal();
+
+// Clear the command on refresh/load
+var cmdInput = document.getElementById('command');
+
+onload = () => {
+  cmdInput.value = ''
+}
+
+// Keep command line focused when clicking anywhere
+document.body.addEventListener('mouseup', () => {
+  cmdInput.focus();
+});
+
 // Command list
 const commands = {
   help: `These are the allowed commands:<br>
@@ -8,36 +44,27 @@ const commands = {
   about: `<p>My name is Justin Tew, and I'm a Marine Corps veteran and a software developer.
   I am primarily proficient in Python, but can learn anything quickly. Some other areas I have gained experience in include
   Javacript, Linux, scripting, DevOps, and networking.</p>`,
+  // Add projects command to output links to projects
 }
-
 const validCmds = Object.keys(commands);
-const cmdInput = document.getElementById('command');
 
-onload = () => {
-  cmdInput.value = ''
-}
-
-// Keep command line focused
-document.body.addEventListener('mouseup', () => {
-  cmdInput.focus();
-});
-
-// Accept a command
-const cmdLines = document.getElementsByName('terminal');
-
-cmdInput.addEventListener('keypress', (event) => {
+// On pressing Enter, don't go to new line, check command and output, if valid
+document.body.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
     let cmd = cmdInput.value;
     if (validCmds.includes(cmd)) {
-      cmdInput.setAttribute('readonly', 'readonly');
-      let outputText = commands[cmd];
-      let outputDiv = document.createElement('div');
-      outputDiv.setAttribute('id', 'output');
-      outputDiv.innerHTML = outputText;
-      document.body.append(outputDiv);
+      renderOutput(cmd);
     }
-    let newLine = cmdLines.item(0).cloneNode(true);
-    document.body.append(newLine);
+  // Disable the text area after command is entered
+  cmdInput.setAttribute('readonly', 'readonly');
+  // Remove ID
+  cmdInput.removeAttribute('id');
+  // Create a new terminal line with the same ID and select/focus it
+  mkTerminal();
+  cmdInput = document.getElementById('command');
+  cmdInput.focus();
   }
 });
+
+
