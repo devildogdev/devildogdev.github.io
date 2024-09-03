69e3a06d2cdff179644e6a9554e07f7d6de7e89f
devildogdev <justinrtew@gmail.com>
2024-09-02 22:33:21 -0500

    tweaked a little

---
+++ script.js
@@ -0,0 +1,43 @@
+const gh = "https://github.com/devildogdev";
+const projects = [
+  {
+    name: "warlord",
+    desc: "A spin on the 1984 game Drug Wars, written in Go",
+  },
+  {
+    name: "genpass",
+    desc: "A simple password generator in Go",
+  },
+  {
+    name: "ptlog",
+    desc: "An htmx web app that puts form input into a table",
+  },
+  {
+    name: "curses-typing-test",
+    desc: "A typing test CLI",
+  },
+  {
+    name: "iss_location",
+    desc: "ISS Location Flask web app that uses an API",
+  },
+];
+
+const getProjects = () => {
+  for (var i; i < projects.length; i++) {
+    let p = projects[i];
+    let url = `${gh}/${p.name}.git`;
+    let pDiv = document.getElementById("projects");
+    p.append(`
+    <li>
+        <span class="orange">-</span>
+        <span class="syntax"> [</span>
+        <span class="green">"${p.desc}"</span>
+        <span class="syntax">](</span>
+        <a href="${url}" target="_blank">${url}</a>
+        <span class="syntax">)</span>
+    </li>
+    `);
+  }
+};
+
+document.onload(getProjects());
